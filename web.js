28a487e811f6a0e8de0b3a52192c8e9cecc0b1b9
Stump <stumpbeard@gmail.com>
2017-07-04 13:41:42 -0400

    Another attempt at heroku

--- web.js
+++ web.js
@@ -1,9 +1,4 @@
-var port = Number(process.env.PORT || 8000);
-
 var express = require('express');
 var app = express();
-
 app.use(express.static(__dirname + '/app'));
-var server = app.listen(port, function() {
-  console.log('Listening on port %d', server.address().port);
-});
+app.listen(process.env.PORT || 3000);
